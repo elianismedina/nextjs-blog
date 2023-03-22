e521f6d1c393e35f69eb5d09fb39256bae2010fb
Elianis Medina <elianismedina05@outlook.com>
2023-03-21 20:38:01 -0500

    changed favicon icon

--- components/layout.js
+++ components/layout.js
@@ -11,10 +11,7 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-       <link rel="manifest" href="/site.webmanifest">
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
           content="Take a look at Elianis Medina resume and personal blog"

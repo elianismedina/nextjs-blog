c3252aac70627f7696fb23403179b97aed86a848
Elianis Medina <elianismedina05@outlook.com>
2023-03-21 20:12:49 -0500

    changed introductory statement

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>[Hello, my name is Elianis and I am a team coach with vast experience leading high-performance teams in customer service]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
